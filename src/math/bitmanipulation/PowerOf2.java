39562a4fce20b7ff6ef61ff9ba827a8669ec64f5
bhushanstuti <65156483+bhushanstuti@users.noreply.github.com>
2020-05-11 21:27:58 +0530

    Bit Manipulation Program 1: Compute power of 2 Handle overflow of result for greater values on input N

--- src/math/bitmanipulation/PowerOf2.java
+++ src/math/bitmanipulation/PowerOf2.java
@@ -1,12 +1,22 @@
 package math.bitmanipulation;
 
 public class PowerOf2 {
-    public static void main(String[] args){
-        int num = 5;
-        System.out.println(computePowerOf2(num));
+    public static void main(String[] args) {
+        int num1 = 5;
+        //will only work till n = 30
+        System.out.println(computePowerOf2(num1));
+        int num2 = 30;
+        System.out.println(computePowerOf2(num2));
+
+        int num3 = 31;
+        System.out.println(computePowerOf2Long(num3));
     }
 
-    public static int computePowerOf2(int num){
-        return 1<<num;
+    public static int computePowerOf2(int num) {
+        return 1 << num;
+    }
+
+    public static long computePowerOf2Long(int num) {
+        return 1L << num;
     }
 }
